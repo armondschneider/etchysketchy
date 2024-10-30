23201f74e1a98c6a04d74dcac190f3cdccb386b8
Armond Schneider <mondoloves@icloud.com>
2024-10-30 14:22:28 -0700

    Initial Commit

---
+++ etchysketchy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  etchysketchy
+//
+//  Created by Armond Schneider on 10/30/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ etchysketchy/etchysketchyApp.swift
@@ -0,0 +1,17 @@
+//
+//  etchysketchyApp.swift
+//  etchysketchy
+//
+//  Created by Armond Schneider on 10/30/24.
+//
+
+import SwiftUI
+
+@main
+struct etchysketchyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
